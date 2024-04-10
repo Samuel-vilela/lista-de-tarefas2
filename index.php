2bb41439833254fd146748bace808c5427f30f2c
Samuel-vilela <146647141+Samuel-vilela@users.noreply.github.com>
2024-04-09 22:22:00 -0300

    atualização

--- index.php
+++ index.php
@@ -1,6 +1,49 @@
-<?php include "cabecalho.php"?>
+<?php include "cabecalho.php" ?>
 
 <h1>📌Tarefas</h1>
-<a href="/" class="btn btn-primary">adicionar tarefas</a>
+<a href="novo-formulario.php" class="btn btn-primary">adicionar tarefas</a>
 
-<?php include "rodape.php"?>
+<table class="table p-3 bg-primary bg-opacity-10 border border-info border-start-0 rounded-end">
+    <tr>
+        <th>&nbsp;</th>
+        <th>ID</th>
+        <th>Titulo</th>
+        <th>Descrição</th>
+        <th>Status</th>
+    </tr>
+    <?php
+    include "conexao.php";
+    $sql = "select * from tarefas order by status asc, id desc";
+    $resultado = mysqli_query($conexao, $sql);
+
+    while ($umaTarefa = mysqli_fetch_assoc($resultado)) :
+    ?>
+        <tr>
+            <td>
+                <?php
+                if ($umaTarefa['status'] == 0){
+                    ?>
+                    <a href= 'editar-salvar.php' class="btn">✔</a>''
+                    <?php
+                }
+                ?>
+            </td>
+            <td><?= $umaTarefa['id']; ?></td>
+            <td><?= $umaTarefa['titulo'] ?></td>
+            <td><?= $umaTarefa['descricao'] ?></td>
+            <td>
+                <?php
+                if ($umaTarefa['status'] == 1) {
+                    echo "completo";
+                } else {
+                    echo "pendente";
+                }
+                ?>
+            </td>
+        <tr>
+        <?php
+    endwhile;
+        ?>
+</table>
+
+<?php include "rodape.php" ?>
